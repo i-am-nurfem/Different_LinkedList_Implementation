3e96e8580c516061127fde53944354efb96b3860
iamnurfemtaysi <t.nurfem@gmail.com>
2022-07-22 21:19:10 +0300

    checked linked based set and array based set functionality inside main()

--- src/IntNode.java
+++ src/IntNode.java
@@ -2,6 +2,11 @@
 public class IntNode {
     public int data;
     public  IntNode next;
+
+    public IntNode() {
+        this.next = null;
+    }
+
     public IntNode(int val) {
         this.data=val;
         this.next=null;
